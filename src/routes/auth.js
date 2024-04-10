d9316323a7935a3c3ad8c950176df036d00acc5e
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-04-10 12:24:23 +0200

    Token trouvé, fix problem split

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { signin, signup } from "../controller/auth.js";
-
-// import { body } from 'express-validator';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
--- src/routes/phones.js
+++ src/routes/phones.js
@@ -11,13 +11,13 @@ import { body } from "express-validator";
 
 const router = express.Router();
 
-// GET http://localhost:3001/cars
+// GET http://localhost:3001/phones
 router.get("/", getPhones);
 
-// GET http://localhost:3001/cars/1
+// GET http://localhost:3001/phones/1
 router.get("/:id", getPhone);
 
-// POST http://localhost:3001/cars
+// POST http://localhost:3001/phones
 router.post(
   "/",
   [
@@ -27,12 +27,8 @@ router.post(
   createPhone
 );
 
-// PUT http://localhost:3001/cars/1 creer une route qui
-// permet de modiier une voiture
 router.put("/:id", udpatePhone);
 
-// DELETE http://localhost:3001/cars/1 creer une route qui
-// permet de supprimer une voiture
 router.delete("/:id", deletePhone);
 
 // ERROR
